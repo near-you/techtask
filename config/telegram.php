2aa9fe8adb3d7764dd92c694fc03989f9142c2b0
andrii <andrii.romanyshyn@jetsoftpro.com>
2022-12-09 10:51:18 +0200

    setup ngrok sail artisan telegram:webhook trelloTasksBot --setup

--- config/telegram.php
+++ config/telegram.php
@@ -29,7 +29,7 @@ return [
     |             ]
     */
     'bots'                         => [
-        'mybot' => [
+        'trelloTasksBot' => [
             'token'               => env('TELEGRAM_BOT_TOKEN', 'YOUR-BOT-TOKEN'),
             'certificate_path'    => env('TELEGRAM_CERTIFICATE_PATH', 'YOUR-CERTIFICATE-PATH'),
             'webhook_url'         => env('TELEGRAM_WEBHOOK_URL', 'YOUR-BOT-WEBHOOK-URL'),
